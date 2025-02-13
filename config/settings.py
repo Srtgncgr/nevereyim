53b31ab35d87f0b442a39139f3bdf0ba526323af
Serhat Gençgör <gencgorserhat@gmail.com>
2025-02-13 18:24:44 +0300

    yyeyeee

--- config/settings.py
+++ config/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = 'django-insecure-^v02zo!(+iv%3#!($^r_#t@^55#i+$c0t$^@46atjqc(pi4bw9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nevereyim-production.up.railway.app', 'https://nevereyim-production.up.railway.app'] 
-CSRF_TRUSTED_ORIGINS = ['https://nevereyim-production.up.railway.app']
+ALLOWED_HOSTS = ['nevereyim.shop', 'https://nevereyim.shop'] 
+CSRF_TRUSTED_ORIGINS = ['https://nevereyim.shop']
 
 
 # Application definition
